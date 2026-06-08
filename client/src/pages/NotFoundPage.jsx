2622e563fe0513d789dc2a85b1950772d3f4ccd5
AnmolMehra435 <gtrdemon66@gmail.com>
2026-06-08 20:26:55 +0530

    added proper navigation for not found pages and unauthorized pages

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,15 +1,23 @@
 import Skeleton from "@/components/layout/Skeleton"
 import { Helmet } from "react-helmet-async"
+import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router-dom"
 
 function NotFoundPage(){
+    const navigate = useNavigate();
+
+    const redirect = () => {
+        navigate("/");
+    }
     return(
         <>
             <Helmet>
                 <title>Page Not Found</title>
             </Helmet>
             <Skeleton>
-                <div className="flex justify-center items-center h-screen">
+                <div className="flex flex-col gap-8 justify-center items-center h-screen">
                     <h1 className='text-4xl text-red-500'>Page Not Found</h1>
+                    <Button vairant="destructive" onClick={redirect}>Go Home</Button>
                 </div>
             </Skeleton>
         </>
--- client/src/pages/UnauthorizedPage.jsx
+++ client/src/pages/UnauthorizedPage.jsx
@@ -1,15 +1,24 @@
 import Skeleton from "@/components/layout/Skeleton"
+import { Button } from "@/components/ui/button"
 import { Helmet } from "react-helmet-async"
+import { useNavigate } from "react-router-dom"
 
 function UnauthorizedPage(){
+
+    const navigate = useNavigate();
+
+    const redirect = () => {
+        navigate("/")
+    }
     return(
         <>
          <Helmet>
             <title>Unauthroized</title>
          </Helmet>
             <Skeleton>
-                <div className="flex justify-center items-center h-screen">
+                <div className="flex flex-col justify-center gap-8 items-center h-screen">
                     <h1 className='text-4xl text-red-500'>UnAuthroized Page</h1>
+                    <Button className="bg-gray-700 text-white"  variant="destructive" onClick={redirect}>Go Home</Button>
                 </div>
             </Skeleton>
         </>
